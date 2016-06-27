70cfc87d074d7f7d014e89bb184471bcb509ce89
lanou3g <609544164@qq.com>
2016-06-27 09:10:09 +0800

    个人信息界面搭建

---
+++ EarBook/EarBook/Classes/Scenes/User/Controller/UserTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserTableViewController.h
+//  EarBook
+//
+//  Created by lanou3g on 16/6/25.
+//  Copyright © 2016年 赵符壹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserTableViewController : UITableViewController
+
+@end
