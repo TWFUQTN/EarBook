67e06ba3852ea2f6efe4e119614b7223d10d7a48
lanou3g <609544164@qq.com>
2016-06-29 09:41:49 +0800

    声音详情页

---
+++ EarBook/EarBook/Classes/Scenes/Detail/Controller/VoiceDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VoiceDetailViewController.h
+//  EarBook
+//
+//  Created by lanou3g on 16/6/29.
+//  Copyright © 2016年 赵符壹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoiceDetailViewController : UIViewController
+
+@end
