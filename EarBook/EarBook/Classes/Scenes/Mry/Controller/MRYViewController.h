f6ea9a0beab0faeac1f76d5829097a06af6e5859
lanou3g <609544164@qq.com>
2016-06-30 21:45:39 +0800

    may导入

---
+++ EarBook/EarBook/Classes/Scenes/Mry/Controller/MRYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRYViewController.h
+//  EarBook
+//
+//  Created by lanou3g on 16/6/30.
+//  Copyright © 2016年 赵符壹. All rights reserved.
+//
+
+#import "MryScrollPageVC.h"
+
+@interface MRYViewController : MryScrollPageVC
+
+@end
